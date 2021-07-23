eb474d35691696194e7057bcc503c7db1ad176ef
Linda Johnstone <linda.johnstone@myob.com>
2021-07-23 10:43:06 +1000

    Add Messages class -  string constants
    All tests passing

---
+++ Blackjack/Messages.cs
@@ -0,0 +1,10 @@
+namespace Blackjack
+{
+    public class Messages
+    {
+        public const string PlayerWins = "You beat the Dealer!";
+        public const string DealerWins = "Dealer wins!";
+        public const string Score = "You are currently at {0} with the hand {1}.";
+        public const string Bust = "Bust!";
+    }
+}
