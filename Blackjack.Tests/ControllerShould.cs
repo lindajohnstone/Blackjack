405ccfcb6225017e93ce5f358ff1a2c426d85f9b
Linda Johnstone <linda.johnstone@myob.com>
2021-08-04 09:50:55 +1000

    GameResultShould - dealer wins if player bust, player wins if dealer bust
    1 test failing

--- Blackjack.Tests/ControllerShould.cs
+++ Blackjack.Tests/ControllerShould.cs
@@ -24,7 +24,7 @@ namespace Blackjack.Tests
         }
 
         [Fact]
-        public void ReturnDealerWinOutcome_GivenPlayerGoesBust()
+        public void ReturnDealerWin_GivenPlayerGoesBust()
         {
             /*
                 player receives 2 cards from deck
@@ -48,6 +48,51 @@ namespace Blackjack.Tests
             _controller.Play();
 
             Assert.Equal("Dealer wins!", _mockOutput.ToString()); // failing
+        }
+
+        [Fact]
+        public void ReturnDealerWin_GivenPlayerBust()
+        {
+            var output = new StubOutput();
+            var player = new Player(new Hand());
+            var dealer = new Dealer(new Hand()); 
+            var controller = new Controller(_mockInput.Object, output, player, dealer, _mockDeck.Object);
+            _mockInput.SetupSequence(_ => _.ReadLine())
+                .Returns("1")
+                .Returns("0");
+            _mockDeck.SetupSequence(d => d.DealCard())
+                .Returns(new Card(CardRank.Eight, CardSuit.Hearts))
+                .Returns(new Card(CardRank.Ten, CardSuit.Diamonds))
+                .Returns(new Card(CardRank.Two, CardSuit.Hearts))
+                .Returns(new Card(CardRank.Two, CardSuit.Clubs))
+                .Returns(new Card(CardRank.Four, CardSuit.Diamonds));
+
+            controller.Play();
+
+            Assert.Equal("Dealer wins!", output.GetLastOutput()); 
+        }
+
+        [Fact]
+        public void ReturnPlayerWin_GivenDealerBust()
+        {
+            var output = new StubOutput();
+            var player = new Player(new Hand());
+            var dealer = new Dealer(new Hand());
+            var controller = new Controller(_mockInput.Object, output, player, dealer, _mockDeck.Object);
+            _mockInput.SetupSequence(_ => _.ReadLine())
+                .Returns("1")
+                .Returns("0");
+            _mockDeck.SetupSequence(d => d.DealCard())
+                .Returns(new Card(CardRank.Eight, CardSuit.Hearts))// player
+                .Returns(new Card(CardRank.Ten, CardSuit.Diamonds))// player
+                .Returns(new Card(CardRank.Ten, CardSuit.Hearts))// dealer
+                .Returns(new Card(CardRank.Six, CardSuit.Clubs))// dealer
+                .Returns(new Card(CardRank.Two, CardSuit.Diamonds))// player - score 20
+                .Returns(new Card(CardRank.Six, CardSuit.Diamonds));
+
+            controller.Play();
+
+            Assert.Equal("You beat the Dealer!", output.GetLastOutput());
         }
     }
 }
---
+++ Blackjack.Tests/StubOutput.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace Blackjack.Tests
+{
+    public class StubOutput : IOutput
+    {
+        public List<string> OutputList { get; private set; } = new List<string>();
+
+        public void Write(string value)
+        {
+            OutputList.Add(value);
+        }
+
+        public void WriteLine(string value)
+        {
+            OutputList.Add(value);
+        }
+
+        public string GetLastOutput()
+        {
+            return OutputList[^1];
+        }
+    }
+}
